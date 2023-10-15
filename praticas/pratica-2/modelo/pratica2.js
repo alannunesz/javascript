56cbb983f0aca323c80526080ac7ac7cae1224d0
Alan Nunes <145228555+alannunesz@users.noreply.github.com>
2023-10-15 15:45:38 -0300

    Exercícios

---
+++ praticas/pratica-2/modelo/pratica2.js
@@ -0,0 +1,11 @@
+var agora = new Date()
+var hora = agora.getHours()
+var hrs = document.getElementById('hrs')
+var img = document.getElementById('img')
+if (hora < 12) {
+    hrs.innerHTML = `Agora são ${horas} horas!`
+} else if (hora === 14:23) {
+    hrs.innerHTML = `Agora são ${horas} horas!`
+} else {
+    hrs.innerHTML = 'A'
+}
